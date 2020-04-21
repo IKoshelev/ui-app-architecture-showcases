8a4f846c8b4f5409cef8326a0df9e05d98e5e026
Paul <water7earth@gmail.com>
2020-04-21 12:19:26 +0200

    adds pseudo Deals.Store

---
+++ src/stores/Deals.Store.ts
@@ -0,0 +1,9 @@
+// properties
+// activeDeal - a uniquie identifier for which deal the user has selected
+// Deals - an array containing the selected values of each deal, each with a unique identifier
+// isLoading
+
+export class DealsStore {
+
+
+}
