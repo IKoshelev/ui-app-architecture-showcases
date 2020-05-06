5b6522b9726311f66919d9320669cdbc8304bc76
IKoshelev <koshelew@live.com>
2020-05-06 11:12:32 +0200

    Branch for starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,17 @@
 import React from 'react';
-import { observer, Observer } from 'mobx-react';
-
-import { CarPurchase } from './screens/car-purchase/components/CarPurchase';
-import { appVm } from './App.VM';
 
 import './App.css';
 
-export const App = observer(() => {
+export const App = () => {
   return <div id='app-root'>
 
     <div className='main-logo'>
-      Welcome to Crazy Ivan Motors (MVVM)
+      Welcome to Crazy Ivan Motors
     </div>
 
     <div className='screens'>
-      <div className='tabs'>
-        <button
-          className='button-add-new-deal'
-          onClick={appVm.addNewDeal}
-        >
-          Add deal
-        </button>
-
-        {
-          appVm.capPurchaseVMs.map(x => (
-            <div
-              className={`deal-tab-header ${x === appVm.activeCapPurchaseVM ? 'active' : ''}`}
-              key={x.id}
-            >
-              <Observer>
-                {() => {
-                  console.log(`rendering header for ${x.id}`);
-                  return (<>
-                    <div
-                      className='header-text'
-                      onClick={() => appVm.setActiveDeal(x)}
-                    >
-                      {x.tabHeader}
-                    </div>
-                  </>);
-                }}
-              </Observer>
-              <button
-                className='close-button'
-                onClick={x.close}
-              >
-                X
-                </button>
-            </div>
-          ))
-        }
-      </div>
-
-      <div className={`active-tab`}>
-        {
-          appVm.activeCapPurchaseVM &&
-          <CarPurchase vm={appVm.activeCapPurchaseVM} />
-        }
-      </div>
 
     </div>
   </div>
 
-});
+};
--- src/util/observable-ticker.ts
+++
@@ -1,9 +0,0 @@
-import { observable, action } from "mobx";
-
-export const ticker1second = observable({
-    lastTickDate: new Date()
-});
-
-setInterval(action(() => {
-    ticker1second.lastTickDate = new Date();
-}), 1000);
