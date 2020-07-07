b2848a8d5ffdb95a29652785aa67529a5d8fcc26
IKoshelev <koshelew@gmail.com>
2020-07-07 10:28:17 -0700

    JS ckeleton + CSS files moved

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -7,51 +7,21 @@
    [reagent.dom  :as rdom]
    ["react" :as react]
 
-   ["/js/api/CarInsurance.Client.js" 
+   ["/js/api/CarInsurance.Client.js"
     :refer [InsurancePlanType carInsuranceClient]
-    :rename {InsurancePlanType insurance-plan-type 
+    :rename {InsurancePlanType insurance-plan-type
              carInsuranceClient car-insurance-client}]
    ;;todo make a wrapper
    ))
 
-(println (js->clj insurance-plan-type))
-
-(def stuff (go (let
-                [resp (try
-                        (<p! (.getAvaliableInsurancePlans car-insurance-client))
-                        (catch js/Error err (println "Ooops, promise had an error" err))
-                        (finally (println "Promise pipe done")))]
-                 (-> resp js->clj println)
-                 resp)))
-
-(defonce perpetual-count (r/atom 0))
-
-(defn Counter [external-counter]
-  (js/console.log "renderning Counter")
-  (let [count (or external-counter (r/atom 0))]
-    (fn [] [:<>
-            [:div (str "I'm a counter of " @count)]
-            [:button {:on-click #(swap! count inc)} "+1"]])))
-
-(defn Counter2 []
-  (let [[count set-count!] (react/useState 0)]
-    (r/as-element
-     [:div
-      [:p "You clicked " count " times"]
-      [:button
-       {:on-click #(set-count! inc)}
-       "Click"]])))
-
+;;todo research loading css via webpack
 
 (defn ^:dev/after-load start []
   (rdom/render
 
-   [:<>
-    [:div "Starting div"]
-    [Counter]
-    [Counter]
-    [Counter perpetual-count]
-    [:> Counter2]]
+   [:div#app-root
+    [:div.main-logo "Welcome to Crazy Ivan Motors"]
+    [:div.screens]]
 
    (js/document.getElementById "app")
 
