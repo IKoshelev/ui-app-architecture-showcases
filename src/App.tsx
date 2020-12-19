3797220e7102e7c2613247148cec86f6955a9570
Ivan Koshelev <koshelew@live.com>
2020-12-19 23:08:34 +0100

    test1

--- src/App.tsx
+++ src/App.tsx
@@ -1,133 +1,43 @@
-import React from 'react';
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-  RecoilState,
-} from 'recoil';
+import React from "react"
+import ReactDOM from "react-dom"
+import { makeAutoObservable, reaction } from "mobx"
+import { observer } from "mobx-react"
 
-import './App.css';
+// Model the application state.
+class Timer {
+    secondsPassed = 0
 
-let uniqueId = 0;
-function getNextUniqueId(){
-  return ++uniqueId;
-}
-
-type Todo = {
-  id: number,
-  text: string,
-  done: boolean
-}
-
-type TodoFilter = 'ALL' | 'DONE' | 'NOT_DONE';
-
-const todosListAtom = atom({
-  key: 'todos_state', // unique ID (with respect to other atoms/selectors)
-  default: [] as RecoilState<Todo>[], // default value (aka initial value)
-});
-
-const todosFilterAtom = atom({
-  key: 'todos_filter', 
-  default: 'ALL',
-});
-
-const todosFilteredSelector = selector({
-  key: 'todos_filtered_view',
-  get: ({get}) => {
-    const filter = get(todosFilterAtom);
-
-    if(filter === 'ALL') {
-      return todosListAtom;
-    } else {
-      const todos = get(todosListAtom);
-      const desiredState = 
-        filter === 'DONE' ? true : 
-        filter === 'NOT_DONE' ? false :
-        (() => {throw new Error(`Unknown filter state ${filter}`)})();
-
-      return todos.filter(x => get(x).done === desiredState);
+    constructor() {
+        makeAutoObservable(this);
+        reaction(
+          () => this.secondsPassed,
+          (seconds) => console.log(seconds)
+        );
     }
-  }
-});
 
-export const App = () => {
-  return <RecoilRoot>
-    <div id='app-root'>
+    increase() {
+        this.secondsPassed += 1
+    }
 
-      <div className='main-logo'>
-        Welcome to Crazy Ivan Motors
-    </div>
+    get secondeDoubled(){
+      return this.secondsPassed * 2;
+    }
 
-      <div className='screens'>
-
-      </div>
-    </div>
-  </RecoilRoot>
-};
-
-
-export const TodoCmp: React.FC<{
-  todoAtom: RecoilState<Todo>
-}> = ({todoAtom}) => {
-  
-  const [todosList, updateTodosList] = useRecoilState(todosListAtom);
-  const [todo, updateTodo] = useRecoilState(todoAtom);
-
-  return <div>
-    <input type="text" 
-          value={todo.text}
-          onChange={(e) => updateTodo({
-            ...todo,
-            text: e.target.value
-          })}
-    />
-    <input type="checkbox"
-        checked={todo.done}
-        onChange={(e) =>  updateTodo({
-          ...todo,
-          done: e.target.checked
-        })}
-    />
-    <div
-      onClick={() => updateTodosList(todosList.filter(x => x !== todoAtom) )}
-    >
-      X
-    </div>
-  </div>;
-};
-
-export const TodoList = () => {
-  
-
-  const todosFiltered = useRecoilValue(todosFilteredSelector);
-
-  return <div>
-    <div>
-      {todosFiltered.map(todoAtom => { 
-        return <TodoCmp key={useRecoilValue(todoAtom).id} todoAtom={todoAtom} />
-      })}
-    </div>
-    <button onClick={() => {
-      const [todosList, updateTodosList] = useRecoilState(todosListAtom);
-      const id = getNextUniqueId();
-      const newTodo = atom<Todo>({
-        key: `todo_${id}`,
-        default: {
-          id,
-          text: 'something to do',
-          done: false
-        }
-      });
-
-      updateTodosList([
-        ...todosList,
-        newTodo
-      ]);
-    }}>
-      Add
-    </button>
-  </div>
+    reset() {
+        this.secondsPassed = 0
+    }
 }
 
+export const myTimer = new Timer()
+
+// Build a "user interface" that uses the observable state.
+export const TimerView = observer(({ timer }: { timer: Timer }) => (
+    <button onClick={() => timer.reset()}>Seconds passed: {timer.secondsPassed}</button>
+))
+
+//ReactDOM.render(<TimerView timer={myTimer} />, document.body)
+
+// Update the 'Seconds passed: X' text every second.
+setInterval(() => {
+    myTimer.increase()
+}, 1000)
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './App';
+import { myTimer, TimerView } from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TimerView timer={myTimer} />
   </React.StrictMode>,
   document.getElementById('root')
 );
