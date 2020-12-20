e024cabfe17bc570d47d4e8fb17304b52fbec042
Ivan Koshelev <koshelew@live.com>
2020-12-20 14:20:15 +0100

    first working prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,72 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
 import { makeAutoObservable, reaction } from "mobx"
 import { observer } from "mobx-react"
+import { IReactComponent } from "mobx-react/dist/types/IReactComponent";
 
-// Model the application state.
-class Timer {
-    secondsPassed = 0
+export const callback = {
+    call: () => {}
+};
 
-    constructor() {
-        makeAutoObservable(this);
-        reaction(
-          () => this.secondsPassed,
-          (seconds) => console.log(seconds)
-        );
-    }
-
-    increase() {
-        this.secondsPassed += 1
-    }
-
-    get secondeDoubled(){
-      return this.secondsPassed * 2;
-    }
-
-    reset() {
-        this.secondsPassed = 0
-    }
+function logReadAccess(object: object, key: string | number | symbol | undefined) {
+    console.log(`Read ${(key ?? "").toString()}`, object);
 }
 
-export const myTimer = new Timer()
+function logWriteAccess(object: object, key: string | number | symbol) {
+    console.log(`Wrote ${key.toString()}`, object);
+    setTimeout(callback.call);
+}
 
-// Build a "user interface" that uses the observable state.
-export const TimerView = observer(({ timer }: { timer: Timer }) => (
-    <button onClick={() => timer.reset()}>Seconds passed: {timer.secondsPassed}</button>
-))
+function makeObservableProxy<T extends object>(initialState: T): T {
+    return new Proxy(initialState as any, {
+        get: function (oTarget, sKey) {
+            logReadAccess(oTarget, sKey);
+            const result =  oTarget[sKey];
+            if (typeof result === 'object') {
+                return makeObservableProxy(result);
+            } 
+            return result;
+        },
+        set: function (oTarget, sKey, vValue) {
+            logWriteAccess(oTarget, sKey);
+            const result = oTarget[sKey] = vValue;
+            if (typeof result === 'object') {
+                return makeObservableProxy(result);
+            } 
+            return result;
+        },
+        deleteProperty: function (oTarget, sKey) {
+            logWriteAccess(oTarget, sKey);
+            return delete oTarget[sKey];
+        },
+        ownKeys: function (oTarget) {
+          logReadAccess(oTarget, undefined);
+          return Object.getOwnPropertyNames(oTarget);
+        },
+        has: function (oTarget, sKey) {
+            logReadAccess(oTarget, sKey);
+            return sKey in oTarget || oTarget.hasItem(sKey);
+        },
+        defineProperty: function (oTarget, sKey, oDesc) {
+            logWriteAccess(oTarget, sKey);
+            return Object.defineProperty(oTarget, sKey, oDesc);
+        },
+      }) as unknown as T;
+}
 
-//ReactDOM.render(<TimerView timer={myTimer} />, document.body)
+export const atom = makeObservableProxy({});
 
-// Update the 'Seconds passed: X' text every second.
-setInterval(() => {
-    myTimer.increase()
-}, 1000)
+// todo implemet all of:
+// getPrototypeOf? (target: T): object | null;
+// setPrototypeOf? (target: T, v: any): boolean;
+// isExtensible? (target: T): boolean;
+// preventExtensions? (target: T): boolean;
+// getOwnPropertyDescriptor? (target: T, p: PropertyKey): PropertyDescriptor | undefined;
+// has? (target: T, p: PropertyKey): boolean;
+// get? (target: T, p: PropertyKey, receiver: any): any;
+// set? (target: T, p: PropertyKey, value: any, receiver: any): boolean;
+// deleteProperty? (target: T, p: PropertyKey): boolean;
+// defineProperty? (target: T, p: PropertyKey, attributes: PropertyDescriptor): boolean;
+// ownKeys? (target: T): PropertyKey[];
+// apply? (target: T, thisArg: any, argArray?: any): any;
+// construct? (target: T, argArray: any, newTarget?: any): object;
