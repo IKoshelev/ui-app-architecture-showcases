4e4d2833b08dead9a75f8b5a7848b4fbf04f6d11
Ivan Koshelev <koshelew@live.com>
2020-12-20 14:20:35 +0100

    +!

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { myTimer, TimerView } from './App';
+import { atom, callback } from './App';
 import * as serviceWorker from './serviceWorker';
+import { useState } from 'react';
+
+(window as any).atom = atom;
+
+const Cmp: React.FC<{
+  atom: any,
+  callback: any
+}> = ({atom, callback}: any) => {
+  const [s, setState] = useState(0);
+  //if(setState.toString() !== 'function () {}'){
+    callback.call = () => {
+      setState((s) => s + 1)
+    };
+  //}
+  console.log('rendering');
+  return <>
+    {JSON.stringify(atom)}
+    <button onClick={() => (atom as any).a = 5}>+</button>
+    <button onClick={() => setState((s) => s + 1)}>+</button>
+  </>;
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <TimerView timer={myTimer} />
-  </React.StrictMode>,
+  <Cmp atom={atom} callback={callback} />,
   document.getElementById('root')
 );
 
