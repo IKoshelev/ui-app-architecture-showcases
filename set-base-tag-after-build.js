59903fcb7f79ebd3ccb990b6e65a407aed35c383
Ivan Koshelev <koshelew@gmail.com>
2023-01-09 21:15:23 +0100

    small fixes

--- set-base-tag-after-build.js
+++ set-base-tag-after-build.js
@@ -4,6 +4,8 @@ const filePath = './dist/index.html';
 
 let text = fs.readFileSync(filePath, { encoding: 'utf8'});
 
-text = text.replace('<head>', '<head>\r\n  <base href="https://uiappashowcases.blob.core.windows.net/$web/solidjs/">');
+text = text.replace('<head>', '<head>\r\n  <base href="https://ikoshelev.github.io/ui-app-architecture-showcases/solidjs/">');
+
+text = text.replaceAll(`="/assets/`, `="./assets/`)
 
 fs.writeFileSync(filePath, text, { encoding: 'utf8'});
